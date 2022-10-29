64ef41911da8595c90cd20e9a886a8835b31ffb4
Waldemar Henszke <pol7di@MacBook-Pro-Waldemar.local>
2022-10-29 17:59:14 +0200

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,14 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-	let token
+	let token, accounts, deployer
 
 	beforeEach(async () => {
 	  const Token = await ethers.getContractFactory('Token')
 	  token = await Token.deploy('Dapp University','DAPP','1000000')
+
+	  accounts = await ethers.getSigners()
+	  deployer = accounts[0]
 	})
 
 	describe('Deployment', () => {
@@ -18,8 +21,9 @@ describe('Token', () => {
 		const symbol = 'DAPP'
 		const decimals = '18'
 		const totalSupply = tokens('1000000')
-		it('has correct name', async () => {
-	  expect( await token.name()).to.equal(name)
+
+it('has correct name', async () => {
+	  expect(await token.name()).to.equal(name)
 	})
 
 it('has correct symbol', async () => {
@@ -31,11 +35,13 @@ it('has correct decimals', async () => {
 	})
 
 it('has correct total supply', async () => {
-	  const value = tokens('1000000')
 	  expect(await token.totalSupply()).to.equal(totalSupply)
 	})
 
+it('assigns total supply to deployer', async () => {
+	expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 	})
 
-})
+  })
 
+})
